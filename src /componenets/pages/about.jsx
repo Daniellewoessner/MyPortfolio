ce416eaa5fc8b9f07e53532e87cdcffe287e3f4e
Daniellewoessner <dlegare7792@gmail.com>
2025-01-21 14:06:15 -0500

    change image route

--- src /componenets/pages/about.jsx
+++ src /componenets/pages/about.jsx
@@ -20,7 +20,7 @@ export default function About() {
               />
               <c>
               <img
-               src={import.meta.env.BASE_URL + "/Add a heading.png"}
+               src={ "/Add a heading (2).png"}
               alt="Danielle Woessner"
               className="relative w-20 h-20 md:w-5 md:h-5 rounded-full object-cover shadow-xl border-4 border-white mx-auto"
               
