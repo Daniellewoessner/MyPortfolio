c0a94072f06b4d957ea48719a016e6d0ee99d8bf
Daniellewoessner <dlegare7792@gmail.com>
2025-04-14 11:49:36 -0400

    import updated

--- src /componenets/Navtabs.jsx
+++ src /componenets/Navtabs.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import '../styles/NavTabs.css'; // Adjust the path as necessary
+import '../../src /styles/Navtabs.css'; // Adjust the path as necessary
 
 function NavTabs() {
   const currentPage = useLocation().pathname;
